66f496fba1d55ec303d9758d413677fb1e2678f4
Victor Kovalchuk <vict02r05@gmail.com>
2026-06-02 16:27:16 +0300

    creating the app component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
+import './style.css';
+
 function App() {
   return (
-    <div>Today I learned</div>
+    <header className="header">
+      <div className="logo">
+        <img
+          src="logo.png"
+          height="68"
+          width="68"
+          alt="Today I Learned Logo"
+        />
+        <h1>Today I Learned</h1>
+      </div>
+
+      <button className="btn btn-large btn-open">Share a fact</button>
+    </header>
   );
 }
 
