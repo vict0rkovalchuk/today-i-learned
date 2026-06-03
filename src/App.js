037e32c88c8ec16506a9e4cf3fe04ee0ee497977
Victor Kovalchuk <vict02r05@gmail.com>
2026-06-03 17:56:47 +0300

    creating a loader component

--- src/App.js
+++ src/App.js
@@ -7,18 +7,30 @@ import supabase from './services/supabase';
 
 import './style.css';
 import { useEffect, useState } from 'react';
+import Loader from './components/Loader';
 
 function App() {
   const [showForm, setShowForm] = useState(false);
   const [facts, setFacts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(function () {
     async function getFacts() {
+      setIsLoading(true);
+
       const { data: facts, error } = await supabase
         .from('facts')
-        .select('*');
+        .select('*')
+        .order('votesInteresting', { ascending: false })
+        .limit(1000);
 
-      setFacts(facts);
+      if(!error) {
+        setFacts(facts);
+      } else {
+        alert('There was problem getting data');  
+      }
+
+      setIsLoading(false);
     }
 
     getFacts();
@@ -36,7 +48,8 @@ function App() {
 
       <main className="main">
         <CategoryFilter />
-        <FactsList facts={facts} />
+
+        {isLoading ? <Loader /> : <FactsList facts={facts} />} 
       </main>
     </div>
   );
