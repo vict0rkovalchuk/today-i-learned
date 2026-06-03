a38156d004a9aecccebacd935dde68e200da30c0
Victor Kovalchuk <vict02r05@gmail.com>
2026-06-03 17:10:53 +0300

    working with forms the react way

--- src/components/NewFactForm.js
+++ src/components/NewFactForm.js
@@ -1,14 +1,49 @@
+import { useState } from "react";
+import { CATEGORIES } from "../data/categories";
+
 export default function Form() {
+  const [text, setText] = useState('');
+  const [source, setSource] = useState('');
+  const [category, setCategory] = useState('');
+  
+  const textLength = text.length;
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
-    <form className="fact-form">
-      <input type="text" placeholder="Share a fact with the world..." />
-      <span>200</span>
-      <input type="text" placeholder="Trustworthy source..." />
-      <select>
+    <form 
+      className="fact-form"
+      onSubmit={handleSubmit}
+    >
+      <input 
+        type="text" 
+        placeholder="Share a fact with the world..." 
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <span>{200 - textLength}</span>
+      <input 
+        type="text" 
+        placeholder="Trustworthy source..." 
+        value={source}
+        onChange={(e) => setSource(e.target.value)}
+      />
+      <select 
+        value={category}
+        onChange={(e) => setCategory(e.target.value)}
+      >
         <option value="">Choose category:</option>
-        <option value="technology">Technology</option>
-        <option value="science">Science</option>
-        <option value="finance">Finance</option>
+
+        {CATEGORIES.map(({ name }) => (
+          <option 
+            value={name} 
+            key={name}
+          >
+            {name[0].toUpperCase() + name.slice(1)}
+          </option>
+        ))}
       </select>
       <button className="btn btn-large">Post</button>
     </form>
